1eaeca2c74f98a358e6d994139729583c07d9fad
etsushin <xiaoxin871213@163.com>
2017-08-30 20:55:58 +0800

    test

--- src/main/java/com/maven/springtest01/service/UserService.java
+++ src/main/java/com/maven/springtest01/service/UserService.java
@@ -24,7 +24,7 @@ public class UserService {
 		return userDao.findUserByUserName(userName);
 	}
 	public void loginSuccess(User user){
-		user.setCredits(user.getCredits()+15);
+		user.setCredits(user.getCredits()+99);
 		LoginLog loginLog = new LoginLog();
 		loginLog.setUserId(user.getUserId());
 		loginLog.setIp(user.getLastIP());
